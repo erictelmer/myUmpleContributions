190b62f7936a3b02bfa884e5696279d5d259caaa
Telmer <telmer@telmer-ThinkPad-T410.(none)>
2014-04-30 19:39:02 -0400

    I think deep History is workinggit ls-files -m | xargs git add

--- testbed/test/cruise/statemachine/test/HistoryStatesTest.java
+++ testbed/test/cruise/statemachine/test/HistoryStatesTest.java
@@ -15,4 +15,18 @@ public class HistoryStatesTest
     course.toHistory();
     Assert.assertEquals(HistoryCourse.SmS1.Ss2, course.getSmS1());
   }
+  
+  @Test
+  public void SimpleDeepHistoryState()
+  {
+    DeepHistoryCourse course = new DeepHistoryCourse();
+    course.toSs2Sss2();
+    course.toSsss2();
+    course.toS2();
+    course.toDeepHistory();
+    System.out.println("After to deepH, SmS1 = " + course.getSmS1());
+    Assert.assertEquals(DeepHistoryCourse.SmS1.Ss2, course.getSmS1());
+    Assert.assertEquals(DeepHistoryCourse.SmS1Ss2.Sss2, course.getSmS1Ss2());
+    Assert.assertEquals(DeepHistoryCourse.SmS1Ss2Sss2.Ssss2, course.getSmS1Ss2Sss2());
+  }
 }

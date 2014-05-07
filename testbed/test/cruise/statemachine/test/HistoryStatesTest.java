d7fcd280749c2b5624c5fa0799a79401f92f0f2b
Telmer <telmer@telmer-ThinkPad-T410.(none)>
2014-05-07 19:32:24 -0400

    deep history working with errors if no substates

--- testbed/test/cruise/statemachine/test/HistoryStatesTest.java
+++ testbed/test/cruise/statemachine/test/HistoryStatesTest.java
@@ -6,6 +6,25 @@ import org.junit.Test;
 public class HistoryStatesTest
 {
   @Test
+  public void AutoTransitionToDeepHistory()
+  {
+      DeepHistoryCourse course = new DeepHistoryCourse();
+      course.toSs2Sss2();
+      course.toSsss1();
+      course.toS2Ss2();
+      Assert.assertEquals(DeepHistoryCourse.SmS1Ss2Sss2.Ssss1, course.getSmS1Ss2Sss2());
+  }
+  
+  
+  @Test
+  public void ToUninitializedDeepHistory()
+  {
+    DeepHistoryCourse course = new DeepHistoryCourse();
+    course.toS2HStar();
+    Assert.assertEquals(DeepHistoryCourse.SmS2.Ss1, course.getSmS2());
+  }
+  
+  @Test
   public void SimpleHistoryState()
   {
     HistoryCourse course = new HistoryCourse();
@@ -24,7 +43,6 @@ public class HistoryStatesTest
     course.toSsss2();
     course.toS2();
     course.toDeepHistory();
-    System.out.println("After to deepH, SmS1 = " + course.getSmS1());
     Assert.assertEquals(DeepHistoryCourse.SmS1.Ss2, course.getSmS1());
     Assert.assertEquals(DeepHistoryCourse.SmS1Ss2.Sss2, course.getSmS1Ss2());
     Assert.assertEquals(DeepHistoryCourse.SmS1Ss2Sss2.Ssss2, course.getSmS1Ss2Sss2());
